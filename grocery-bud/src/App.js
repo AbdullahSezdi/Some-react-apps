db2e24877a6a1cc0332f4e99f83ce8635b38529c
Abdullah Sezdi <abdullahsezeies@gmail.com>
2022-12-06 18:51:50 +0300

    .

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,30 +1,64 @@
 
 import './App.css';
 import {useEffect,useState} from "react"
+import { v4 as uuidv4 } from 'uuid';
+import {FaEdit,FaRegTrashAlt,FaRegSave} from "react-icons/fa"
+
 
 function App() {
 
-  const [list,setList]=useState([""]);
-  const [text,setText]=useState();
-  const [show,setShow]=useState(false)
 
-
+  const [list,setList]=useState([ ]);
+  const [text,setText]=useState("");
+  const [editText,setEditText]=useState("");
+  const [editing,setIsEditing]=useState(false)
+  
   const addBasket=()=>{
-    list.push(text)
-    setShow(!show)
+    setList(prevList=>[...prevList,{
+      id:uuidv4(),
+      todo:text,
+      isChecked:false,
+      isEditable:false,
+    }]
+    )
+    setEditText(text)
+    setText("")
 
   }
+
+  const editItem=(id)=>{
+    list.map((item=>item.id===id & setEditText(item.todo)))
+    setList(prevList=>prevList.map(item=>item.id===id ? ({...item,isEditable:!item.isEditable,todo:editText}):item))
+    
+   
+    
+
+  }
+  const saveItem=(id)=>{
+    
+    setList(prevList=>prevList.map(item=>item.id===id ? ({...item,todo:editText,isEditable:false}):item))
+  
+
+  }
+ 
   
   return (
     <div className="App">
     <div className="container">
       <h2>Grocery Bud</h2>
       <form>
-        <input onChange={(event)=>{setText(event.target.value)}} placeholder="e.g. eggs"></input>
+        <input value={text}  onChange={(event)=>{setText(event.target.value)}} placeholder="e.g. eggs"></input>
         <button onClick={addBasket} type="button">Submit</button>
-        {show ? list.map((e)=>{return <p>{e}</p>}):setShow(!show)}
-        
       </form>
+
+      {list.map((todoItem,index)=>{return <div key={index}>
+        <div> {!todoItem.isEditable ? todoItem.todo :  <input value={todoItem.todo} onChange={(event)=>setEditText(event.target.value)}></input>}
+            
+        </div>
+        {!todoItem.isEditable ? <FaEdit onClick={()=>editItem(todoItem.id)}></FaEdit>:<FaRegSave onClick={()=>saveItem(todoItem.id)}></FaRegSave>}      <FaRegTrashAlt></FaRegTrashAlt>
+        
+     </div>})}
+
       
     </div>
       
