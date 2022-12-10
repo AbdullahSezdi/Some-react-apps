42ee520b1e2e67052d2730c6a831dcf270432baa
Abdullah Sezdi <abdullahsezeies@gmail.com>
2022-12-10 20:20:52 +0300

    .

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,28 +1,45 @@
 import "./App.css"
-import { FaFacebook,FaTwitter,FaLinkedin } from "react-icons/fa";
+import { FaFacebook,FaTwitter,FaLinkedin,FaBars} from "react-icons/fa";
+import { useState } from "react";
+
 
 
 function App() {
+
+  const [show,setShow]=useState(true)
+
+  const toggleLinks=()=>{
+    setShow(!show)
+  }
+
   return (
     <div className="App">
       <div className="navbar">
         <img className="logo" src="https://logos-world.net/wp-content/uploads/2022/02/Trader-Joes-Logo-700x394.png"></img>
-        <div className="category">
+        <button className="toggle" onClick={toggleLinks}><FaBars/></button>
+        {show ? (
+          <div className="flex-item"> <div className="category">
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Project</a>
           <a href="#">Contact</a>
-          <a href="#">Profile</a>
+          <a href="#">Profile</a> 
         </div>
         <div className="social-media">
-        <a href="htpps://wwww.facebook.com"><FaFacebook/></a>
+        <a href="wwww.facebook.com" target="_blank" ><FaFacebook/></a>
         <a href="wwww.twitter.com"><FaTwitter/></a>
         <a href="wwww.linkedin.com"><FaLinkedin/></a>
-        
-        
-        
 
-        </div>
+        </div> </div>
+       ): <div className="category">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Project</a>
+          <a href="#">Contact</a>
+          <a href="#">Profile</a> 
+        </div>}
+        
+        
       </div>
      
     </div>
